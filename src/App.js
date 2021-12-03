4e5dc4403689206cc585cb11a1b5543e4034f4ec
Wyxuch <50527152+Wyxuch@users.noreply.github.com>
2021-12-03 23:44:33 +0100

    Add initial content layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import bg from "./bg.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage: `url(${bg})`, backgroundSize: '25%'}}>
+        <div id='main'>
+            <h1>Tu będzie zagadka z kodem</h1>
+        </div>
     </div>
   );
 }
